dc0d3465eb8cb7a7567b339ec2a79ed06e9a8c19
Kevin Leung <leung.kevin23@gmail.com>
2016-10-03 15:35:01 -0500

    removed node_module dir

--- luis.js
+++ luis.js
@@ -17,7 +17,7 @@ var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
-  
+
 // Create chat bot
 var connector = new builder.ChatConnector({
     appId: process.env.APP_ID,

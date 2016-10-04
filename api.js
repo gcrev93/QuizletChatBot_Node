e51476e738577582ca35e99996886c538ebd8672
gcrev93 <gcrev93@gmail.com>
2016-10-04 16:24:22 -0500

    changes to formatting

--- api.js
+++ api.js
@@ -23,31 +23,34 @@ exports.GetSets = function (user, callback) {
             else {
                 body = JSON.parse(body);
                 for (var x = 0; x < body.length; x++) {
-                    sets = sets + body[x].title + ',';
+                    if (x == body.length - 1)
+                        sets = sets + body[x].title;
+                    else
+                        sets = sets + body[x].title + ', ';
+
                     table[body[x].title] = body[x].id; //creating a hash table to store set names and IDs
                 }
                 console.log('Got sets');
                 exports.Sets = sets;
-               /* setTimeout(function () {
-                    exports.Sets = sets;
-                    callback(null, sets);
-                }, 3000)*/
-                
+                /* setTimeout(function () {
+                     exports.Sets = sets;
+                     callback(null, sets);
+                 }, 3000)*/
+
             }
         })
 }
 
 exports.GetTerms = function (key, callback) {
     request.get({
-        uri: 'https://api.quizlet.com/2.0/sets/'+ table[key] + '?client_id=X46hm4RZVz&whitespace=1',
+        uri: 'https://api.quizlet.com/2.0/sets/' + table[key] + '?client_id=X46hm4RZVz&whitespace=1',
     },
         function (error, response, body) {
             if (error)
                 console.log(error);
             else {
                 body = JSON.parse(body);
-                for(var x = 0; x < body.terms.length; x++)
-                {
+                for (var x = 0; x < body.terms.length; x++) {
                     terms.push(body.terms[x].term)
                     def.push(body.terms[x].definition);
                 }
